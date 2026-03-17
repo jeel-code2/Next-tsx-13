a613faa51f2b50f52e85d5f189b8cd5e373bd8b0
Justice <jeelcode5@gmail.com>
2026-03-17 12:52:34 +0300

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import NavBar from '@/components/NavBar'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Geist } from 'next/font/google'
 import Footer from '@/components/Footer'
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={inter.className}>
         <NavBar />
         {children}
